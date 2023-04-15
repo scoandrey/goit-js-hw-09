87cbd2607be8f6bc61088df8762fc6cdcdefef7c
scoandrey <32551449+scoandrey@users.noreply.github.com>
2023-04-15 02:30:37 +0000

    Deploying to gh-pages from @ scoandrey/goit-js-hw-09@d531544d1fd39ba047dd7e08efc31bad5af91ac9 🚀

---
+++ 01-color-switcher.39ba03d2.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("[data-start]"),r=document.querySelector("[data-stop]");let d=null;r.setAttribute("disabled",!0),e.addEventListener("click",(function(a){e.setAttribute("disabled",!0),r.removeAttribute("disabled",!0),d=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),r.addEventListener("click",(function(){r.setAttribute("disabled",!0),e.removeAttribute("disabled",!0),clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.39ba03d2.js.map
